7c9a83b284a8edb8fad573df3a94e318bcc58ce6
joachimgood <joachim.good@hotmail.com>
2017-03-01 21:04:52 +0100

    Final

---
+++ brollop2017/src/brollop2017/Controllers/EmailSender.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace brollop2017.Controllers
+{
+    public class EmailSender
+    {
+        public void send()
+        {
+            
+        }
+    }
+}
---
+++ brollop2017/src/brollop2017/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using brollop2017.Controllers;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace brollop2017
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult foljet()
+        {
+            return View();
+        }
+
+        public IActionResult historia()
+        {
+            return View();
+        }
+
+        public IActionResult toastmasters()
+        {
+            EmailSender email = new EmailSender();
+            //email.send();
+            return View();
+        }
+
+
+
+    }
+}
